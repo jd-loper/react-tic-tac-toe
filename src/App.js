6b747e8ccc6d59bd26782a478b4aea6318f95890
Joseph Loper <jd.loper@icloud.com>
2023-11-12 13:42:23 -0700

    Bug fix

--- src/App.js
+++ src/App.js
@@ -53,10 +53,10 @@ function Board({ xIsNext, squares, onPlay }) {
 }
 
 export default function Game() {
-  const xIsNext = currentMove % 2 === 0;
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[currentMove];
   const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
